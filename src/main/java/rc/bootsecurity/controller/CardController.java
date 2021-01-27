5af789a07122f6860e7d65582aa31bc9f9d6afa0
Bart <Bartmax112@gmail.com>
2021-01-27 02:33:07 +0100

    commit

--- src/main/java/rc/bootsecurity/controller/CardController.java
+++ src/main/java/rc/bootsecurity/controller/CardController.java
@@ -6,10 +6,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import rc.bootsecurity.db.CardRepository;
 import rc.bootsecurity.db.UserRepository;
 import rc.bootsecurity.model.Card;
@@ -40,7 +37,13 @@ public class CardController {
         return "redirect:/profile/form";
     }
 
+    @GetMapping("/deleteCard/{id}")
+    public String deleteCard(@PathVariable(value = "id") long id) {
 
+        // call delete employee method
+        this.cardService.deleteCardById(id);
+        return "redirect:/profile/form";
+    }
 
 
 }
